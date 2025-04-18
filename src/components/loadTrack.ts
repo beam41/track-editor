27a1270ca2544755d263c89fcc58bde644d9754d
Freeman <4031216+fmnxl@users.noreply.github.com>
2025-04-18 16:28:03 +0700

    support loading track from uri

--- src/components/loadTrack.ts
+++ src/components/loadTrack.ts
@@ -2,7 +2,50 @@ import { fileInput, trackJson, loadTrackBtn, downloadBtn, clipboardBtn } from 's
 import { global } from 'src/global';
 import { drawMap, zoomFit } from './map';
 
+const PROXY_URL = 'https://www.aseanmotorclub.com/proxy';
+
+function maybeFetchTrackUri(proxy = PROXY_URL) {
+  const params = new URLSearchParams(window.location.search);
+  const uri = params.get('uri');
+  if (!uri) {
+    return Promise.resolve(null);
+  }
+  const proxiedUri = `${proxy}?url=${encodeURIComponent(uri)}`;
+  return fetch(proxiedUri)
+    .then((response) => {
+      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+      return response.json();
+    })
+    .catch((error) => {
+      console.error('Fetch error:', error);
+    });
+}
+
+function loadTrack() {
+  const text = trackJson.value;
+  try {
+    global.trackData = JSON.parse(text);
+    global.selectedIndex = null;
+    drawMap();
+    zoomFit();
+    downloadBtn.disabled = false;
+    clipboardBtn.disabled = false;
+    window.scrollTo(0, document.body.scrollHeight);
+  } catch (e) {
+    console.error('loadTrackBtn click failed:', e);
+    alert('Invalid JSON text.');
+  }
+}
+
 export function initEvent() {
+  maybeFetchTrackUri().then((track) => {
+    if (track) {
+      trackJson.value = JSON.stringify(track, null, 4);
+      // TODO: remove hack, instead listen to map load event before loading track
+      setTimeout(loadTrack, 1000);
+    }
+  });
+
   fileInput.addEventListener(
     'input',
     () => {
@@ -26,25 +69,7 @@ export function initEvent() {
     },
   );
 
-  loadTrackBtn.addEventListener(
-    'click',
-    function () {
-      const text = trackJson.value;
-      try {
-        global.trackData = JSON.parse(text);
-        global.selectedIndex = null;
-        drawMap();
-        zoomFit();
-        downloadBtn.disabled = false;
-        clipboardBtn.disabled = false;
-        window.scrollTo(0, document.body.scrollHeight);
-      } catch (e) {
-        console.error('loadTrackBtn click failed:', e);
-        alert('Invalid JSON text.');
-      }
-    },
-    {
-      passive: true,
-    },
-  );
+  loadTrackBtn.addEventListener('click', loadTrack, {
+    passive: true,
+  });
 }
